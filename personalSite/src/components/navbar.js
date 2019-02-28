3298c0bcdd805c3865caa3ac4b2c6e1bb3a7e9d6
Marcus Henriksbø <mthenrik@stud.ntnu.no>
2019-02-28 23:57:35 +0100

    Start

---
+++ personalSite/src/components/navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    render() {
+        <div>
+            <p> Marcus Henriksbø</p>
+
+
+        </div>
+
+
+    }
+
+
+}
